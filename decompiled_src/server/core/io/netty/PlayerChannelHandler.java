651627080130cddb47d8e80fee0748979696cf8a
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-14 17:00:37 +0100

    Add decompiled java classes

---
+++ decompiled_src/server/core/io/netty/PlayerChannelHandler.java
@@ -0,0 +1,33 @@
+package com.hypixel.hytale.server.core.io.netty;
+
+import com.hypixel.hytale.protocol.Packet;
+import com.hypixel.hytale.server.core.io.PacketHandler;
+import com.hypixel.hytale.server.core.io.adapter.PacketAdapters;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class PlayerChannelHandler extends ChannelInboundHandlerAdapter {
+   private final PacketHandler handler;
+
+   public PlayerChannelHandler(PacketHandler handler) {
+      this.handler = handler;
+   }
+
+   public PacketHandler getHandler() {
+      return this.handler;
+   }
+
+   public void channelInactive(ChannelHandlerContext ctx) {
+      this.handler.logCloseMessage();
+      this.handler.closed(ctx);
+   }
+
+   public void channelRead(ChannelHandlerContext ctx, Object msg) {
+      if (ctx.channel().isActive()) {
+         Packet packet = (Packet)msg;
+         if (!PacketAdapters.__handleInbound(this.handler, packet)) {
+            this.handler.handle(packet);
+         }
+      }
+   }
+}
